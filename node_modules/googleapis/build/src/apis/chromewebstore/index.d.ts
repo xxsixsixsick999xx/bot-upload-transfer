de919a6e33b7f0d0816dac2890141a35462572e6
xxsixsixsick999xx <email_yang_terdaftar_di_githubmu@example.com>
2025-10-24 15:50:23 +0700

    Clean history - remove credentials.json permanently

---
+++ node_modules/googleapis/build/src/apis/chromewebstore/index.d.ts
@@ -0,0 +1,12 @@
+/*! THIS FILE IS AUTO-GENERATED */
+import { AuthPlus } from 'googleapis-common';
+import { chromewebstore_v1_1 } from './v1.1';
+export declare const VERSIONS: {
+    'v1.1': typeof chromewebstore_v1_1.Chromewebstore;
+};
+export declare function chromewebstore(version: 'v1.1'): chromewebstore_v1_1.Chromewebstore;
+export declare function chromewebstore(options: chromewebstore_v1_1.Options): chromewebstore_v1_1.Chromewebstore;
+declare const auth: AuthPlus;
+export { auth };
+export { chromewebstore_v1_1 };
+export { AuthPlus, GlobalOptions, APIRequestContext, GoogleConfigurable, StreamMethodOptions, MethodOptions, BodyResponseCallback, } from 'googleapis-common';
